f8dcef42f1afcf3e3dbd0a8d4d10f70a79accf4c
Guoliang Cao <gcao99@gmail.com>
2012-12-12 09:42:27 -0500

    Scrap player's rank history

---
+++ lib/scrap-rank-history.rb
@@ -0,0 +1,44 @@
+require 'mechanize'
+require 'json'
+
+agent = Mechanize.new
+page = agent.get('http://www.atpworldtour.com/Rankings/Singles.aspx')
+
+is_first = true
+i = 1
+page.search('.rankingsContent tr').each do |row|
+  break if i <= 0
+  i -= 1
+
+  if is_first
+    is_first = false
+    next
+  end
+
+  rank = row.search('td:first-child span').text.to_i
+  last, first = row.search('td:first-child a').text.split(',')
+  first = first[1..-1].strip
+  last = last.strip
+  history = []
+  url = row.search('td:first-child a').href + '?t=rh'
+  puts url
+  history_page = agent.get(url)
+  is_first_history = true
+  history_page.search('.bioHistoryTable tr').each do |row|
+    if is_first_history
+      is_first_history = false
+      next
+    end
+    day, month, year = row.search('td:first-child').text.split('.')
+    break if year.to_i <= 2002
+    rank_then = row.search('td:nth-child(2)').text.to_i
+    history << ["#{month}/#{day}/#{year}", rank_then]
+  end
+  
+  result = {first: first, last: last, rank: rank, history: history}
+
+  File.open("rank-history-#{rank}.js", 'w') do |f|
+    f.puts "var history#{rank} = #{result.to_json};"
+  end
+end
+
