89b82a08fff137f67ccf034a1eea861333ff8af6
Guoliang Cao <gcao99@gmail.com>
2012-12-13 09:27:55 -0500

    Rank history

--- lib/scrap-rank-history.rb
+++ lib/scrap-rank-history.rb
@@ -5,7 +5,7 @@ agent = Mechanize.new
 page = agent.get('http://www.atpworldtour.com/Rankings/Singles.aspx')
 
 is_first = true
-i = 1
+i = 2
 page.search('.rankingsContent tr').each do |row|
   break if i <= 0
   i -= 1
@@ -16,12 +16,11 @@ page.search('.rankingsContent tr').each do |row|
   end
 
   rank = row.search('td:first-child span').text.to_i
-  last, first = row.search('td:first-child a').text.split(',')
-  first = first[1..-1].strip
-  last = last.strip
+  #last, first = row.search('td:first-child a').text.split(',')
+  #first = first[1..-1].strip
+  #last = last.strip
   history = []
-  url = row.search('td:first-child a').href + '?t=rh'
-  puts url
+  url = row.search('td:first-child a').first.attr('href') + '?t=rh'
   history_page = agent.get(url)
   is_first_history = true
   history_page.search('.bioHistoryTable tr').each do |row|
@@ -32,13 +31,15 @@ page.search('.rankingsContent tr').each do |row|
     day, month, year = row.search('td:first-child').text.split('.')
     break if year.to_i <= 2002
     rank_then = row.search('td:nth-child(2)').text.to_i
+    rank_then = 50 if rank_then <= 0 or rank_then > 50
     history << ["#{month}/#{day}/#{year}", rank_then]
   end
   
-  result = {first: first, last: last, rank: rank, history: history}
+  #result = {first: first, last: last, rank: rank, history: history}
 
   File.open("rank-history-#{rank}.js", 'w') do |f|
-    f.puts "var history#{rank} = #{result.to_json};"
+    #f.puts "var history#{rank} = #{result.to_json};"
+    f.puts "var history#{rank} = #{history.to_json};"
   end
 end
 
